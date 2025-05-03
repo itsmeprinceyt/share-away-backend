2ba1772344ace1faf8078d006ec785f9534368fb
itsmeprinceyt <mohammadbadshah92@gmail.com>
2025-05-04 04:34:02 +0530

    1.18.27 | Removed nodemailer package and fixed typo

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,7 +3,7 @@ import { User } from '../types/User';
 
 /**
  * @brief To fetch user data from the database using their email.
- * @description A user service helper function for login-reset.controller.ts to help
+ * @description A user service helper function for login.controller.ts to help
  * login using email.
 */
 
